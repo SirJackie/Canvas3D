f6cb94cf9a4e6d55aeed3d68225f99186f2a9f7c
lyj00912 <3122742760@qq.com>
2020-01-01 20:38:19 +0800

    refactoring cver2sver() and sver2cver()

--- points.js
+++ points.js
@@ -1,6 +1,4 @@
 //const
-//var everyDegree = (2 * Math.PI) / 360;
-var everyDegree = 1;
 //spherical coordinate
 function sphericalVertex(rho,phi,theta){
     this.rho = rho;
@@ -17,13 +15,13 @@ function cartesianVertex(x,y,z){
 }
 function cver2sver(cver){
     var rho   = Math.sqrt(cver.x*cver.x + cver.y*cver.y + cver.z*cver.z);
-    var phi   = Math.acos(everyDegree * (cver.z / rho));
-    var theta = Math.atan(everyDegree * (cver.y / cver.x));
+    var phi   = Math.acos(cver.z / rho);
+    var theta = Math.atan(cver.y / cver.x);
     return new sphericalVertex(rho,phi,theta);
 }
 function sver2cver(sver){
-    var x = sver.rho * Math.sin(everyDegree * (sver.phi)) * Math.cos(everyDegree * (sver.theta));
-    var y = sver.rho * Math.sin(everyDegree * (sver.phi)) * Math.sin(everyDegree * (sver.theta));
-    var z = sver.rho * Math.cos(everyDegree * (sver.phi));
+    var x = sver.rho * Math.sin(sver.phi) * Math.cos(sver.theta);
+    var y = sver.rho * Math.sin(sver.phi) * Math.sin(sver.theta);
+    var z = sver.rho * Math.cos(sver.phi);
     return new cartesianVertex(x,y,z);
 }
