98158f2479ed0972ea84d2d3df3d3cad5c3f1342
lyj00912 <3122742760@qq.com>
2020-01-01 20:35:05 +0800

    create cver2sver() and sver2cver() convert functions

--- points.js
+++ points.js
@@ -1,3 +1,6 @@
+//const
+//var everyDegree = (2 * Math.PI) / 360;
+var everyDegree = 1;
 //spherical coordinate
 function sphericalVertex(rho,phi,theta){
     this.rho = rho;
@@ -11,4 +14,16 @@ function cartesianVertex(x,y,z){
     this.y = y;
     this.z = z;
     return this;
+}
+function cver2sver(cver){
+    var rho   = Math.sqrt(cver.x*cver.x + cver.y*cver.y + cver.z*cver.z);
+    var phi   = Math.acos(everyDegree * (cver.z / rho));
+    var theta = Math.atan(everyDegree * (cver.y / cver.x));
+    return new sphericalVertex(rho,phi,theta);
+}
+function sver2cver(sver){
+    var x = sver.rho * Math.sin(everyDegree * (sver.phi)) * Math.cos(everyDegree * (sver.theta));
+    var y = sver.rho * Math.sin(everyDegree * (sver.phi)) * Math.sin(everyDegree * (sver.theta));
+    var z = sver.rho * Math.cos(everyDegree * (sver.phi));
+    return new cartesianVertex(x,y,z);
 }
